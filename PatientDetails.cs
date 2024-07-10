4f3ebbb66be20a3aaf7d6e4541183528ed6a3198
Yagna Satwik <satwikkolapalli@gmail.com>
2024-07-10 17:34:06 +0530

    Day10HPM File committed

---
+++ PatientDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day10HPM
+{
+    internal abstract class PatientDetails
+    {
+        public string PatientName { get; set; }
+        //public int InPatientID;
+        public int PatientAge { get; set; }
+        public string PatientDOB { get; set; }
+        public string PatientAddress { get; set; }
+        public string ReferringDoctor { get; set; }
+
+
+
+        protected PatientDetails()
+        {
+            
+        }
+
+
+        public abstract void AddPatientDetails();
+
+        public abstract void GetPatientDetails();
+        public abstract void RemovePatientDetails();
+
+    }
+}
